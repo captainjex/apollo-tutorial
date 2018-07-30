e0a8f8f230237a9c107ec70eef014525f0baad71
wkwksama <mailpakgasing@gmail.com>
2018-07-30 23:42:05 +0700

    engine performance testing

--- server.js
+++ server.js
@@ -2,16 +2,32 @@ import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
+import compression from 'compression';
+import { ApolloEngine } from 'apollo-engine';
 
 const GRAPHQL_PORT = 3000;
+const ENGINE_API_KEY = 'service:tes-apollo-tutorial:GulTOCKbYQlJ5FPM1aYkQw'; // TODO
 
 const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+const engine = new ApolloEngine({
+  apiKey: ENGINE_API_KEY,
+});
+
+// graphQLServer.use(engine.expressMiddleware());
+graphQLServer.use(compression());
+
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema, tracing: true }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-graphQLServer.listen(GRAPHQL_PORT, () =>
-  console.log(
-    `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
-  )
-);
+// Instead of app.listen():
+engine.listen({
+  port: GRAPHQL_PORT,
+  graphqlPaths: ['/graphql'],
+  expressApp: graphQLServer,
+  launcherOptions: {
+    startupTimeout: 3000,
+  },
+}, () => {
+  console.log(`GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`);
+});
